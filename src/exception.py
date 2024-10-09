d032f7684e3dff6013a84660c321b2322d6f6cd6
Monalisa Mohapatra <lisasoro1989@gmail.com>
2024-10-09 13:16:42 +0530

    Pipeline

--- src/exception.py
+++ src/exception.py
@@ -1,22 +1,25 @@
+# handles all the exceptions that arise on runtime
+# whenever we get an exeption , the code will take it to logger.py and make directories on the exceptions 
+# using logging.info
+
+
 import sys
+import logging
 
-
+# we want to show our own custom message when an error is raised.
+# error_detail:sys means error should in sys library.
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name[{0}] line number[{1}] error message[{2}]".format(
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    # exc_tb tells on which file and on which line number the exception occured 
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
     file_name,exc_tb.tb_lineno,str(error))
-
     return error_message
 
-
-
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self,error,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message= error_message_detail(error_message,error_detail=error_detail)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
-    
     def __str__(self):
         return self.error_message
-    
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+# # This has all the common functionalities that we can use in the entire project can use
 import os
 import sys
 
@@ -9,8 +10,10 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
+import pickle
 
 def save_object(file_path,obj):
+    ''' Function is responsible for saving file as pkl'''
     try:
         dir_path = os.path.dirname(file_path)
 
@@ -39,7 +42,7 @@ def evaluate_models( X_train,y_train,X_test,y_test,models,param):
             model.fit(X_train,y_train)
 
 
-            #model.fit(X_train,y_train)
+            #model.fit(X_train,y_test)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
@@ -49,6 +52,13 @@ def evaluate_models( X_train,y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+            
     except Exception as e:
         raise CustomException(e,sys)
 
